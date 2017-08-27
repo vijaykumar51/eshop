50dd77ce2b3b057f92d5fc46b96587dc2a1ae7a0
vijaykumar51 <vijay.kumar.em51@gmail.com>
2017-08-27 12:45:22 +0530

    adding clean, war plugin in pom and changing log levels for project

--- src/main/java/com/eshop/controller/BaseController.java
+++ src/main/java/com/eshop/controller/BaseController.java
@@ -1,5 +1,7 @@
 package com.eshop.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,16 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class BaseController {
 	
+	final static Logger logger = LoggerFactory.getLogger(BaseController.class);
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
-	
+		logger.debug("inside welcome funciton");
 		model.addAttribute("message", "Welcome User");
 		return "index";
 	}
 	
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
 	public String welcomeName(@PathVariable String name, ModelMap model) {
-		
+		logger.debug("inside welcome name function");
+		logger.info("new welcome name tttttttttttttt");
 		model.addAttribute("message", "Welcome " + name);
 		return "index";
 	}
