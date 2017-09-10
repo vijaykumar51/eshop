7e1beecf5007863340773501b8738aadd7f0d1b6
vijaykumar51 <vijay.kumar.em51@gmail.com>
2017-09-10 16:27:03 +0530

    removing unused imports

--- src/main/java/com/eshop/dao/UserDetailsDao.java
+++ src/main/java/com/eshop/dao/UserDetailsDao.java
@@ -1,9 +1,5 @@
 package com.eshop.dao;
 
-import java.util.List;
-
-import com.eshop.model.UserRegistrationDetails;
-
 public interface UserDetailsDao {
 
 	//List<UserRegistrationDetails> findUserByName(String t);
--- src/main/java/com/eshop/dao/UserDetailsImpl.java
+++ src/main/java/com/eshop/dao/UserDetailsImpl.java
@@ -1,19 +1,9 @@
 package com.eshop.dao;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-
 import com.eshop.dao.UserDetailsDao;
-import com.eshop.model.UserRegistrationDetails;
 
 public class UserDetailsImpl implements UserDetailsDao{
 
-	@Autowired
-	private MongoOperations mongo;
 	
 //	@Override
 //	public List<UserRegistrationDetails> findUserByName(String name) {
